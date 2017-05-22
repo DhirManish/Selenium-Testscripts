d80f9f450262187b985226f9c6e74452a2d725be
DhirManish <manish.dhir4@gmail.com>
2017-05-22 11:29:08 +0530

    Added LOC to imput values and click on search icon

--- Testsuite/src/flow/Test.java
+++ Testsuite/src/flow/Test.java
@@ -1,5 +1,8 @@
 package flow;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.firefox.*;
@@ -9,14 +12,22 @@ public class Test {
 
 	public static void main(String[] args){
 		
-		System.setProperty("webdriver.gecko.driver" , "D:\\Java Selenium\\geckodriver-v0.16.1-win64\\geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver" , "D:\\Gecko 16\\geckodriver.exe");
 		
 		WebDriver drv = new FirefoxDriver();
 		
 		drv.get("https://www.google.com");
 		
-		drv.quit();
+		//drv.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
+		//drv.findElement(By.id("hplogo")).click();
+		
+		drv.findElement(By.xpath("/html/body/div/div[3]/form/div[2]/div[2]/div[1]/div[1]/div[3]/div/div/div[3]/div/input[3]")).sendKeys("Automation Testing");
+				
+		drv.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		drv.findElement(By.className("sbico-c")).click();
+				   
 	}
 	
 }
